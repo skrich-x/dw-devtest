c3a352b480d337df980757aa0a01d19f3ef53db6
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-05 18:35:55 -0400

    Added console log to determine when alert is being called

--- scripts/main.js
+++ scripts/main.js
@@ -25,7 +25,7 @@
     var regex = /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
     if (email.val() === '' || !regex.test($email.val()))
       {
-        // alert('Please enter a valid email address.');
+        console.log('Please enter a valid email address.');
         return false;
       }
 
