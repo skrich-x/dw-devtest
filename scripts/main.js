aca51584399225e85d3074959c61b97d8d568e3d
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-05 11:50:20 -0400

    Added placeholder to form input field -autofocus to first field in form

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@
          $("#shipping-company").show();
      });
 
+  ///Validate Number Keypress For Phone Number
      $("#phone").keypress(function (event) {
        //do not display if not Number
         if (event.which !== 8 && event.which !== 0 && (event.which < 48 || event.which > 57)) {
@@ -16,6 +17,8 @@
         return false;
        }
       });
+
+  ///
 
   });
 })();
