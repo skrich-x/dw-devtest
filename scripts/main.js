26aeb286f5db66f23569151e7b23ec41c3d239e0
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-05 00:07:46 -0400

    added JS to hide and show company field on radio button press

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,10 @@
 
   $(document).ready(function(){
     $('body').prepend(JST.application());
+
+    $("#shipping-company").hide();
+     $("#commercial").click(function(){
+         $("#shipping-company").show();
+     });
   });
 })();
