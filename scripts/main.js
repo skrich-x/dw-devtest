fec28a95d87e61b5f70da4a0fb3b2799c1eafec7
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-05 00:57:15 -0400

    Impliment keypress event for numbers only Phone Input -created error span for Numbers only -styled error red

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,14 @@
      $("#commercial").click(function(){
          $("#shipping-company").show();
      });
+
+     $("#phone").keypress(function (event) {
+       //do not display if not Number
+        if (event.which !== 8 && event.which !== 0 && (event.which < 48 || event.which > 57)) {
+
+        return false;
+       }
+      });
+
   });
 })();
